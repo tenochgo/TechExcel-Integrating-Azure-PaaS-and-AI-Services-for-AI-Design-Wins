8ef8c46b038f2217c65025d25f999fc62f2753da
Tenoch <122833419+tenochgo@users.noreply.github.com>
2024-09-26 17:31:38 -0600

    Update env_variables.sh

--- env_variables.sh
+++ env_variables.sh
@@ -1,2 +1,1 @@
-SQLCONNSTR_ContosoSuites='Server=tcp:wy4elfev5k5ji-sqlserver.database.windows.net,1433;Initial Catalog=ContosoSuitesBookings;Persist Security Info=False;User ID=contosoadmin;Password=give100percent%;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;'
-export SQLCONNSTR_ContosoSuites
+xxx
